2ee79e73d94bca1aeb80ecfbb437d7c06f28189e
nguyentuanvu <Cunne11112@gmail.com>
2026-03-21 01:31:10 +0700

    Updates

---
+++ _next/static/eOwhrz0AjXisKN4J3Yd8W/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/IS208-Gioi-thieu-nhom-quan-ly-du-an-CNTT//_next/:path+",
+        "destination": "/IS208-Gioi-thieu-nhom-quan-ly-du-an-CNTT/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
